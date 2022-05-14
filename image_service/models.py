fc5ba89e320c1ac52b278b9ca708dd530cc30dbd
jnoyda110500 <jamil.noyda@etechtexas.com>
2022-05-14 18:57:07 +0530

    wip models database schema

--- image_service/models.py
+++ image_service/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class ImageFile(models.Model):
+    """
+    add this class and the following fields
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ("-created_at",)
+
+    file_url = models.FileField()
+    def _str_(self):
+        return self.file_url
+
+    @property
+    def image_url(self):
+        if self.file_url and hasattr(self.file_url, 'url'):
+            return self.file_url.url
+        else:
+            return None
