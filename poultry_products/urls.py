94608a14f8ff98dfccd6aa980c5ccec75d55cbb6
jnoyda110500 <jamil.noyda@etechtexas.com>
2022-05-20 00:08:43 +0530

    urls file udpated

--- poultry_products/urls.py
+++ poultry_products/urls.py
@@ -1,5 +1,17 @@
-from django.urls import path
 from django.urls import include, path
+from poultry_products.views import UpdateView, DeleteView, ListView
 
 urlpatterns = [
+    path(
+        "list/",
+        ListView.as_view(),
+    ),
+    path(
+        "update/<int:id>/",
+        UpdateView.as_view(),
+    ),
+    path(
+        "delete/<int:id>/",
+        DeleteView.as_view(),
+    ),
 ]
