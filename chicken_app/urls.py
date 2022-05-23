1f4ae11a438c1d06a8d3cfb48f442f337e560ee0
jnoyda110500 <jamil.noyda@etechtexas.com>
2022-05-23 10:18:23 +0530

    static file serve code added

--- chicken_app/urls.py
+++ chicken_app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,7 +31,7 @@ urlpatterns = [
 
 
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Chicken Backend"  # default: "Django Administration"
 admin.site.index_title = "Chicken Backend"  # default: "Site administration"
