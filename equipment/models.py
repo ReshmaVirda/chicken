be716e9eed5a8c102abc9615516d3b7625277973
jnoyda110500 <jamil.noyda@etechtexas.com>
2022-05-23 10:16:59 +0530

    equipment apis created

--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.urls import reverse
+from poultry_products.models import Region, Governorate
+class ProductEquipment(models.Model):
+
+    image_file = models.ImageField(
+        upload_to="products/%Y/%m/%d",
+        blank=True,
+        null=True,
+    )
+
+    product_name = models.CharField(max_length=200, db_index=True)
+
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    governorate = models.ForeignKey(Governorate, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    location = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.product_name
--- equipment/serializers.py
+++ equipment/serializers.py
@@ -5,9 +5,15 @@ from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+from rest_framework import serializers
+from equipment.models import  ProductEquipment
+
+class ProductEquipmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ["username", "email", "groups", "first_name", "last_name", "is_active"]
-
-
+        model = ProductEquipment
+        fields = '__all__'
