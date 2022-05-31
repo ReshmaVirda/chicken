8173b0dd2dc85e25377f8f19f4cb17956ac9bfeb
jnoyda110500 <jamil.noyda@etechtexas.com>
2022-05-31 10:20:10 +0530

    models updated

--- feed/models.py
+++ feed/models.py
@@ -1,13 +1,14 @@
+
 from django.db import models
 
 from django.urls import reverse
 from poultry_products.models import Region, Governorate
+from image_service.models import ImageFile
 class ProductFeed(models.Model):
 
-    image_file = models.ImageField(
-        upload_to="products/%Y/%m/%d",
-        blank=True,
-        null=True,
+    image_file = models.OneToOneField(
+        ImageFile,
+        on_delete=models.CASCADE
     )
 
     product_name = models.CharField(max_length=200, db_index=True)
@@ -18,8 +19,8 @@ class ProductFeed(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    governorate = models.ForeignKey(Governorate, on_delete=models.CASCADE)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    governorate = models.ForeignKey(Governorate, on_delete=models.CASCADE,blank=True,null=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE,blank=True,null=True)
     location = models.TextField(blank=True)
 
     class Meta:
