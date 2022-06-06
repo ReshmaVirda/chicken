916aba2c27aaff6d6711543d4b18b4913b29c88f
jnoyda110500 <jamil.noyda@etechtexas.com>
2022-06-06 19:25:34 +0530

    url

--- veterinary_medicine/serializers.py
+++ veterinary_medicine/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from veterinary_medicine.models import VeterinaryMadicine
 
 class VeterinaryMadicineSerializer(serializers.ModelSerializer):
+    get_image_url = serializers.ReadOnlyField()
+
     class Meta:
         model = VeterinaryMadicine
         fields = '__all__'
